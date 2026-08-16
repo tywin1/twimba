8af9b9d441712f813f873e7b668695d05b35a937
tywin1 <akabanesan@gmail.com>
2026-08-16 22:08:47 +1200

    v2 - stretch goals done!
    fully connected to firebase db (reused the leads tracker one) for new tweets, replies, likes and retweets.
    delete also implemented - deleting any tweet isnt realistic, but hey it works.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
@@ -6,7 +5,8 @@ import { getDatabase,
          ref,
          push,
          onValue,
-         remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+         remove,
+         update } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-tracker-7aeae-default-rtdb.firebaseio.com/"
@@ -16,59 +16,118 @@ const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const referenceInDB = ref(database, "handleTweetBtnClick")
 
+let tweetsData = [];
+let tweetIdToDelete = null;
 
 document.addEventListener('click', function(e){
-    if(e.target.dataset.like){
-       handleLikeClick(e.target.dataset.like) 
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
     }
-    else if(e.target.dataset.retweet){
+    else if (e.target.dataset.retweet) {
         handleRetweetClick(e.target.dataset.retweet)
     }
-    else if(e.target.dataset.reply){
+    else if (e.target.dataset.reply) {
         handleReplyClick(e.target.dataset.reply)
     }
-    else if(e.target.id === 'tweet-btn'){
-        handleTweetBtnClick()
+    else if (e.target.dataset.delete) {
+        handleDeleteClick(e.target.dataset.delete)
+    }
+    else if (e.target.id === 'confirm-delete-btn') {
+        const targetTweetObj = tweetsData.find(tweet => tweet.uuid === tweetIdToDelete);
+        if (targetTweetObj) {
+            const specificTweetRef = ref(database, `handleTweetBtnClick/${targetTweetObj.id}`);
+            remove(specificTweetRef);
+        }
+        closeDeleteModal();
+    }
+    else if (e.target.id === 'cancel-delete-btn' || e.target.id === 'modal-overlay') {
+        closeDeleteModal();
+    }
+    else if (e.target.id === 'tweet-btn') {
+        handleTweetBtnClick(e)
+    }
+    else if (e.target.dataset.submitReply) {
+        handleSubmitReplyClick(e.target.dataset.submitReply)
     }
 })
- 
-function handleLikeClick(tweetId){ 
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
 
-    if (targetTweetObj.isLiked){
-        targetTweetObj.likes--
+function closeDeleteModal() {
+    const deleteModal = document.getElementById('delete-modal');
+    if (deleteModal) {
+        deleteModal.classList.add('hidden');
     }
-    else{
-        targetTweetObj.likes++ 
+    tweetIdToDelete = null;
+}
+
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.find(tweet => tweet.uuid === tweetId)
+
+    if (targetTweetObj) {
+        const newLikes = targetTweetObj.isLiked ? targetTweetObj.likes - 1 : targetTweetObj.likes + 1
+        const newIsLiked = !targetTweetObj.isLiked
+
+        const specificTweetRef = ref(database, `handleTweetBtnClick/${targetTweetObj.id}`)
+        
+        update(specificTweetRef, {
+            likes: newLikes,
+            isLiked: newIsLiked
+        })
     }
-    targetTweetObj.isLiked = !targetTweetObj.isLiked
-    render()
 }
 
 function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
+    const targetTweetObj = tweetsData.find(tweet => tweet.uuid === tweetId)
     
-    if(targetTweetObj.isRetweeted){
-        targetTweetObj.retweets--
+    if (targetTweetObj) {
+        const newRetweets = targetTweetObj.isRetweeted ? targetTweetObj.retweets - 1 : targetTweetObj.retweets + 1
+        const newIsRetweeted = !targetTweetObj.isRetweeted
+
+        const specificTweetRef = ref(database, `handleTweetBtnClick/${targetTweetObj.id}`)
+        
+        update(specificTweetRef, {
+            retweets: newRetweets,
+            isRetweeted: newIsRetweeted
+        })
     }
-    else{
-        targetTweetObj.retweets++
-    }
-    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
-    render() 
 }
 
-function handleReplyClick(replyId){
-    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+    function handleReplyClick(replyId){
+        document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+    }
+
+    function handleDeleteClick(tweetId){
+        tweetIdToDelete = tweetId;
+        document.getElementById('delete-modal').classList.remove('hidden');
+    }
+
+function handleSubmitReplyClick(tweetUuid) {
+    const replyInput = document.getElementById(`reply-input-${tweetUuid}`);
+    const replyText = replyInput.value.trim()
+
+    if (replyText) {
+        const targetTweetObj = tweetsData.find(tweet => tweet.uuid === tweetUuid)
+
+        if (targetTweetObj) {
+            const newReplyObj = {
+                handle: `@tywin1`,
+                profilePic:`images/tywin1.jpg`,
+                tweetText: replyText
+            }
+
+            const currentReplies = targetTweetObj.replies ? [...targetTweetObj.replies] : []
+            currentReplies.push(newReplyObj)
+            const specificTweetRef = ref(database, `handleTweetBtnClick/${targetTweetObj.id}`)
+            update(specificTweetRef, {
+                replies: currentReplies
+            })
+            replyInput.value = ''
+        }
+    }
 }
 
 let lastSubmittedValue = "";
 
-function handleTweetBtnClick(){
+function handleTweetBtnClick(event){
     const tweetInput = document.getElementById('tweet-input')
     const currentValue = tweetInput.value.trim();
 
@@ -76,12 +135,11 @@ function handleTweetBtnClick(){
         event.preventDefault();
         return;
     }
-    
+
     if(tweetInput.value) {
-
         lastSubmittedValue = currentValue;
-
-        tweetsData.unshift ({
+        
+        const newTweetObj = {
             handle: `@tywin1`,
             profilePic: `images/tywin1.jpg`,
             likes: 0,
@@ -91,10 +149,9 @@ function handleTweetBtnClick(){
             isLiked: false,
             isRetweeted: false,
             uuid: uuidv4()
-        })
-    render()
-    push(referenceInDB, tweetInput.value)
-    tweetInput.value = ''
+        }
+        push(referenceInDB, newTweetObj)
+        tweetInput.value = ''
     }
 }
 
@@ -104,20 +161,17 @@ function getFeedHtml(){
     tweetsData.forEach(function(tweet){
         
         let likeIconClass = ''
-        
         if (tweet.isLiked){
             likeIconClass = 'liked'
         }
         
         let retweetIconClass = ''
-        
         if (tweet.isRetweeted){
             retweetIconClass = 'retweeted'
         }
         
         let repliesHtml = ''
-        
-        if(tweet.replies.length > 0){
+        if(tweet.replies && tweet.replies.length > 0){
             tweet.replies.forEach(function(reply){
                 repliesHtml+=`
 <div class="tweet-reply">
@@ -133,7 +187,6 @@ function getFeedHtml(){
             })
         }
         
-          
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -146,7 +199,7 @@ function getFeedHtml(){
                     <i class="fa-regular fa-comment-dots"
                     data-reply="${tweet.uuid}"
                     ></i>
-                    ${tweet.replies.length}
+                    ${tweet.replies ? tweet.replies.length : 0}
                 </span>
                 <span class="tweet-detail">
                     <i class="fa-solid fa-heart ${likeIconClass}"
@@ -160,10 +213,22 @@ function getFeedHtml(){
                     ></i>
                     ${tweet.retweets}
                 </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-trash"
+                    data-delete="${tweet.uuid}"
+                    ></i>
+                </span>
             </div>   
         </div>            
     </div>
     <div class="hidden" id="replies-${tweet.uuid}">
+    <div class="reply-input-area">
+    <input type="text"
+    placeholder="Tweet your reply..."
+    id="reply-input-${tweet.uuid}"
+    class="reply-input">
+    <button class="reply-btn" data-submit-reply="${tweet.uuid}">Reply</button>
+    </div>
         ${repliesHtml}
     </div>   
 </div>
@@ -177,13 +242,26 @@ function render(){
 }
 
 onValue(referenceInDB, function(snapshot) {
-    const snapshotDoesExist = snapshot.exists()
-    if (snapshotDoesExist) {
+    if (snapshot.exists()) {
         const snapshotValues = snapshot.val()
-        const tweebs = Object.values(snapshotValues)
-        render(tweebs)
-    }
-})
+                const cleanTweets = Object.entries(snapshotValues)
+            .filter(function([key, tweet]) {
+                return tweet && typeof tweet === 'object' && tweet.tweetText;
+            })
+            .map(function([key, tweet]) {
+                if (!tweet.replies) {
+                    tweet.replies = [];
+                }
+                return {
+                    ...tweet,
+                    id: key
+                };
+            })
 
-render()
+        tweetsData = cleanTweets.reverse()
+    } else {
+        tweetsData = []; 
+    }
+    render()
+})
 
